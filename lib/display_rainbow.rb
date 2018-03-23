0eda2142948cbe31c5431be252cb30e0198daaeb
Josh Stegmeier <josh@steg.me>
2018-03-23 21:37:16 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,7 +9,7 @@ def display_rainbow(colors)
   print colors[3].chars.first.upcase + ": " + colors[3] + ", "
   print colors[4].chars.first.upcase + ": " + colors[4] + ", "
   print colors[5].chars.first.upcase + ": " + colors[5] + ", "
-  print colors[6].chars.first.upcase + ": " + colors[6]
+  print colors[6].chars.first.upcase + ": " + colors[6] + "\n"
 end
 
 display_rainbow(my_colors)
