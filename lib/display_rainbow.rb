ceb91f69d0a74a085ad16dd163c87bc17e9ae8b6
Josh Stegmeier <josh@steg.me>
2018-03-23 21:32:36 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,13 @@
 # Write your #display_rainbow method here
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+def display_rainbow
+  print colors[0].chars.first.upcase + ": " + colors[0] + ", "
+  print colors[1].chars.first.upcase + ": " + colors[1] + ", "
+  print colors[2].chars.first.upcase + ": " + colors[2] + ", "
+  print colors[3].chars.first.upcase + ": " + colors[3] + ", "
+  print colors[4].chars.first.upcase + ": " + colors[4] + ", "
+  print colors[5].chars.first.upcase + ": " + colors[5] + ", "
+  print colors[6].chars.first.upcase + ": " + colors[6] + ", "
+end
